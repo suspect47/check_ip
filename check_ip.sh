460569c847da3bd7b5ed15979fe6b813fe6180b7
Ilya Igonin <47140422+suspect47@users.noreply.github.com>
2020-09-12 15:20:37 +0300

    Update check_ip.sh

--- check_ip.sh
+++ check_ip.sh
@@ -1,9 +1,13 @@
-until [[ -n "$VALID_IP" ]]
+until [[ -n "$IP_ADDRESS" ]]
 do
 echo "Введите адрес Платформы Нейросс:"
-read IP_ADDRESS
-echo "вот айпи: $IP_ADDRESS"
-VALID_IP=$(echo "$IP_ADDRESS" | egrep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk -F'.' '$1 <=255 && $2 <= 255 && $3 <= 255 && $4 <= 255')
-echo "вот валид айпи: $VALID_IP"
+read IP
+IP_ADDRESS=$(echo "$IP" | egrep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk -F'.' '$1>
+ if [[ -z "$IP_ADDRESS" ]]
+   then
+   echo "Введен некорректный IP-адрес. Попробуйте еще раз!"
+   else
+   :
+ fi
 done
-echo "Будет использован следующий IP-адрес: $VALID_IP"
+echo "Будет использован следующий IP-адрес: $IP_ADDRESS"
